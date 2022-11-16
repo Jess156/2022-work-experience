e8dd702d013e820808f7216079bf48520f7cadaf
Jess156 <60130052+Jess156@users.noreply.github.com>
2022-11-16 21:15:51 +1100

    Update pomodoro.js

--- pages/pomodoro/pomodoro.js
+++ pages/pomodoro/pomodoro.js
@@ -21,14 +21,14 @@ const runTimer = () => {
   const myheading = document.querySelector ("h1");
   myheading.textContent = "in timer";
   } 
-  if (timer < 0, isBreakTime === true) {
-    isBreakTime = false;
-    timer = durationOfWork;
-  } 
-  if (timer < 0, isBreakTime === false) {
-    isBreakTime = true;
-    timer = durationOfBreak;
-  }
+  //if (timer < 0, isBreakTime === true) {
+  //  isBreakTime = false;
+  //  timer = durationOfWork;
+  //} 
+  //if (timer < 0, isBreakTime === false) {
+  //  isBreakTime = true;
+  //  timer = durationOfBreak;
+  //}
   // If the timer is greater or equal to 0
   // then set the timer to 1 second less
   //
